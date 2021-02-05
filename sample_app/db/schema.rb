ea1bc1478521eafaac5fe60263a33b90de01caab
Yuta_Ushizima <contact@yuta-u.com>
2021-02-05 23:37:55 +0900

    feat: Create User

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_112828) do
+ActiveRecord::Schema.define(version: 2021_02_05_143628) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "result"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
