b25ce92b5dccde4bb2e2bca4c80dd487c1b99700
Yuta_Ushizima <contact@yuta-u.com>
2021-02-05 23:42:13 +0900

    feat: Add user_id to Post

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_143628) do
+ActiveRecord::Schema.define(version: 2021_02_05_144045) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +26,5 @@ ActiveRecord::Schema.define(version: 2021_02_05_143628) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
