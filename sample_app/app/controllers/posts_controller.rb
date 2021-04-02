fb1f8c64dc360434df54c6728f5cef46e4c35984
manto-watanabe <wanidrums@gmail.com>
2021-04-02 23:09:03 +0900

    new

--- sample_app/app/controllers/posts_controller.rb
+++ sample_app/app/controllers/posts_controller.rb
@@ -4,11 +4,13 @@ class PostsController < ApplicationController
   end
 
   def draw
-    @post = Post.all.sort_by{rand}.slice(0,10)
+    luckies = ["大吉","中吉","小吉","吉","凶","大凶"]
+    @post = luckies.sample
   end
 end
 
 
+# @post = Post.all.sort_by{rand}.slice(0,10)
 # @post = SELECT * FROM posts ORDER BY RANDOM();
 
 # Post.find_by(id: Random)
